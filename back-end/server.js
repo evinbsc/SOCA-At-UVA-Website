ac8486d235d8b9fef9c6d2e92c8c91e6b7eb3381
Evin St Clair <evin@MacBook-Air-3.local>
2024-07-28 21:19:03 -0700

    Updated Components Folder

--- back-end/server.js
+++ back-end/server.js
@@ -25,5 +25,5 @@ app.get('/executives', async (req, res) => {
   res.json(executives);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
