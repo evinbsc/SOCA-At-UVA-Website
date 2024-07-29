314558c1493e749ca118bd170cac297ad5fd2847
Evin St Clair <evin@MacBook-Air-3.local>
2024-07-28 21:50:21 -0700

    Updated Components Folder

--- front-end/vite-project/src/components/Navbar.jsx
+++ front-end/vite-project/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import '../../style.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="https://drive.google.com/file/d/19wqwQuCQHFnEVmelAMNjHsEc-ITVqb7J/preview" className="rectangle" alt="Logo" />
-      <div>
+      <iframe src="https://drive.google.com/file/d/19wqwQuCQHFnEVmelAMNjHsEc-ITVqb7J/preview" width="30" height="20" className="navbar-logo" alt="SOCA Logo"></iframe>
+      <div className="navbar-links">
         <Link to="/" className="text-wrapper-3">Home</Link>
         <Link to="/discover" className="text-wrapper-4">Discover Caribbean</Link>
         <Link to="/calendar" className="text-wrapper-5">Calendar</Link>
