f9cfa66b45ff44cc8c5bfee80bc47e2bbc46e1f7
Evin St Clair <evin@MacBook-Air-3.local>
2024-08-23 14:51:50 -0400

    Updated Home page

--- front-end/vite-project/src/components/Navbar.jsx
+++ front-end/vite-project/src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 import SOCALogo from '../Assets/SOCALogo.png';
 import '../styles/navbar.css';
 
-
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
-        <img src={SOCALogo} className="rectangle" alt="SOCALogo" />
-      </Link>
-      <div>
+      <div className="logo-container">
+        <Link to="/">
+          <img src={SOCALogo} className="rectangle" alt="SOCALogo" />
+        </Link>
+        <span className="organization-name">Student Organization for Caribbean Awareness</span>
+      </div>
+      <div className="nav-links">
         <Link to="/" className="text-wrapper-3">Home</Link>
         <Link to="/discover" className="text-wrapper-4">Discover Caribbean</Link>
         <Link to="/calendar" className="text-wrapper-5">Calendar</Link>
