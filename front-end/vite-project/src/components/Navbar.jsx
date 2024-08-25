5992fdb0ca18b7abfe3ed25bdd54d92b762f2542
Evin St Clair <evin@MacBook-Air-3.local>
2024-08-25 14:03:17 -0400

    Updated Front-End

--- front-end/vite-project/src/components/Navbar.jsx
+++ front-end/vite-project/src/components/Navbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SOCALogo from '../Assets/SOCALogo.png';
 import '../styles/navbar.css';
 
-
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/">
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776; {/* Unicode for hamburger icon */}
+      </div>
+      <Link to="/" className="logo-container">
         <img src={SOCALogo} className="rectangle" alt="SOCALogo" />
+        <span className="organization-name">Student Organization for Caribbean Awareness</span>
       </Link>
-      <div>
+      <div className={`nav-links ${menuOpen ? 'show' : ''}`}>
         <Link to="/" className="text-wrapper-3">Home</Link>
         <Link to="/discover" className="text-wrapper-4">Discover Caribbean</Link>
         <Link to="/calendar" className="text-wrapper-5">Calendar</Link>
