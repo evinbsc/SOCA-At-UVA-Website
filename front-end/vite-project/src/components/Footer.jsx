def65e7bc8989fe6cbca06f6d4b06099cb8d6c6a
Evin St Clair <evin@MacBook-Air-3.local>
2024-08-25 21:07:11 -0400

    Updated Front-End

--- front-end/vite-project/src/components/Footer.jsx
+++ front-end/vite-project/src/components/Footer.jsx
@@ -4,13 +4,13 @@ import '../styles/footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footer-bottom">
+        All Content Copyright © 2024 University of Virginia SOCA
+      </div>
       <div className="footer-content">
         <p className="footer-statement">
           The Student Organization for Caribbean Awareness (SOCA) comprises members who are University of Virginia students and may include participation from University employees. However, SOCA operates as a separate and independent entity, distinct from the University. It is solely responsible for managing its own activities and affairs. The University of Virginia does not oversee, direct, or control SOCA's operations and holds no responsibility for the organization's contracts, actions, or omissions.
         </p>
-      </div>
-      <div className="footer-bottom">
-        All Content Copyright © 2024 University of Virginia SOCA
       </div>
     </footer>
   );
