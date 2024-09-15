8113137bf0d42d985b28eb2fb06a352aedac3611
Evin St Clair <evin@MacBook-Air-3.local>
2024-09-15 00:50:13 -0400

    Updated Front-End

--- front-end/vite-project/src/components/Footer.jsx
+++ front-end/vite-project/src/components/Footer.jsx
@@ -5,6 +5,14 @@ import '../styles/footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footer-bottom">
+        All Content Copyright © 2024 Student Organization for Caribbean Awareness at University of Virginia
+      </div>
+      <div className="footer-content">
+        <p className="footer-statement">
+          The Student Organization for Caribbean Awareness (SOCA) comprises members who are University of Virginia students and may include participation from University employees. However, SOCA operates as a separate and independent entity, distinct from the University. It is solely responsible for managing its own activities and affairs. The University of Virginia does not oversee, direct, or control SOCA's operations and holds no responsibility for the organization's contracts, actions, or omissions.
+        </p>
+      </div>
       <div className="footer-animation-container">
         <div className="footer-made-by">
           Website made with 💙💛🤍🖤 by Evin St Clair 🇱🇨
@@ -20,14 +28,6 @@ const Footer = () => {
             <img src={linkedinLogo} alt="LinkedIn" className="linkedin-icon" />!
           </a>
         </div>
-      </div>
-      <div className="footer-bottom">
-        All Content Copyright © 2024 Student Organization for Caribbean Awareness at University of Virginia
-      </div>
-      <div className="footer-content">
-        <p className="footer-statement">
-          The Student Organization for Caribbean Awareness (SOCA) comprises members who are University of Virginia students and may include participation from University employees. However, SOCA operates as a separate and independent entity, distinct from the University. It is solely responsible for managing its own activities and affairs. The University of Virginia does not oversee, direct, or control SOCA's operations and holds no responsibility for the organization's contracts, actions, or omissions.
-        </p>
       </div>
     </footer>
   );
